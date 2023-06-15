3eaea37a9c29a8f1d9cd6a61168733f6db6a9455
Justin Britt <britt@uwindsor.ca>
2023-06-15 16:30:44 -0400

    Added visitor

---
+++ HM.HM1B.A.E.O/Visitors/Results/SurgeonNumberAssignedTimeBlocks/BVisitor.cs
@@ -0,0 +1,48 @@
+﻿namespace HM.HM1B.A.E.O.Visitors.Results.SurgeonNumberAssignedTimeBlocks
+{
+    using System.Collections.Generic;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using HM.HM1B.A.E.O.Interfaces.Comparers;
+    using HM.HM1B.A.E.O.Interfaces.IndexElements;
+    using HM.HM1B.A.E.O.Interfaces.ResultElements.SurgeonNumberAssignedTimeBlocks;
+    using HM.HM1B.A.E.O.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Results.SurgeonNumberAssignedTimeBlocks;
+
+    internal sealed class BVisitor<TKey, TValue> : IBVisitor<TKey, TValue>
+        where TKey : IsIndexElement
+        where TValue : IBResultElement
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public BVisitor(
+            INullableValueFactory nullableValueFactory,
+            IOrganizationComparer organizationComparer)
+        {
+            this.NullableValueFactory = nullableValueFactory;
+
+            this.RedBlackTree = new RedBlackTree<Organization, INullableValue<int>>(
+                organizationComparer);
+        }
+
+        private INullableValueFactory NullableValueFactory { get; }
+
+        public bool HasCompleted => false;
+
+        public RedBlackTree<Organization, INullableValue<int>> RedBlackTree { get; }
+
+        public void Visit(
+            KeyValuePair<TKey, TValue> obj)
+        {
+            this.RedBlackTree.Add(
+                obj.Key.Value,
+                this.NullableValueFactory.Create<int>(
+                    obj.Value.Value));
+        }
+    }
+}
