5f6500df9692ebe8c32ecccafde71bbaf6818e98
Justin Britt <britt@uwindsor.ca>
2023-06-16 05:33:35 -0400

    Added visitor

---
+++ HM.HM1B.A.E.O/Visitors/Contexts/PlanningHorizonVisitor.cs
@@ -0,0 +1,48 @@
+﻿namespace HM.HM1B.A.E.O.Visitors.Contexts
+{
+    using System.Collections.Generic;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using HM.HM1B.A.E.O.Interfaces.IndexElements;
+    using HM.HM1B.A.E.O.Interfaces.Indices;
+    using HM.HM1B.A.E.O.InterfacesFactories.IndexElements;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Contexts;
+
+    internal sealed class PlanningHorizonVisitor<TKey, TValue> : IPlanningHorizonVisitor<TKey, TValue>
+        where TKey : INullableValue<int>
+        where TValue : FhirDateTime
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public PlanningHorizonVisitor(
+            ItIndexElementFactory tIndexElementFactory)
+        {
+            this.tIndexElementFactory = tIndexElementFactory;
+
+            this.RedBlackTree = new RedBlackTree<FhirDateTime, ItIndexElement>();
+        }
+
+        private ItIndexElementFactory tIndexElementFactory { get; }
+
+        private It t { get; }
+
+        public bool HasCompleted => false;
+
+        public RedBlackTree<FhirDateTime, ItIndexElement> RedBlackTree { get; }
+
+        public void Visit(
+            KeyValuePair<TKey, TValue> obj)
+        {
+            this.RedBlackTree.Add(
+                obj.Value,
+                this.tIndexElementFactory.Create(
+                    obj.Key.Value.Value,
+                    obj.Value));
+        }
+    }
+}
