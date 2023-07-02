a033bf4ec5b646e1c15cbc79f6e668a870cc2f0c
Justin Britt <britt@uwindsor.ca>
2023-07-02 06:31:14 -0400

    Added factory

---
+++ HM.HM1B.A.E.O/Factories/Contexts/PlanningHorizonVisitorFactory.cs
@@ -0,0 +1,47 @@
+﻿namespace HM.HM1B.A.E.O.Factories.Contexts
+{
+    using System;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using HM.HM1B.A.E.O.Interfaces.Comparers;
+    using HM.HM1B.A.E.O.InterfacesFactories.Contexts;
+    using HM.HM1B.A.E.O.InterfacesFactories.IndexElements;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Contexts;
+    using HM.HM1B.A.E.O.Visitors.Contexts;
+
+    internal sealed class PlanningHorizonVisitorFactory<TKey, TValue> : IPlanningHorizonVisitorFactory<TKey, TValue>
+        where TKey : INullableValue<int>
+        where TValue : FhirDateTime
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public PlanningHorizonVisitorFactory()
+        {
+        }
+
+        public IPlanningHorizonVisitor<TKey, TValue> Create(
+            ItIndexElementFactory tIndexElementFactory,
+            IFhirDateTimeComparer FhirDateTimeComparer)
+        {
+            IPlanningHorizonVisitor<TKey, TValue> instance = null;
+
+            try
+            {
+                instance = new PlanningHorizonVisitor<TKey, TValue>(
+                    tIndexElementFactory,
+                    FhirDateTimeComparer);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ HM.HM1B.A.E.O/InterfacesFactories/Contexts/IPlanningHorizonVisitorFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace HM.HM1B.A.E.O.InterfacesFactories.Contexts
+{
+    using Hl7.Fhir.Model;
+
+    using HM.HM1B.A.E.O.Interfaces.Comparers;
+    using HM.HM1B.A.E.O.InterfacesFactories.IndexElements;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Contexts;
+
+    public interface IPlanningHorizonVisitorFactory<TKey, TValue>
+        where TKey : INullableValue<int>
+        where TValue : FhirDateTime
+    {
+        IPlanningHorizonVisitor<TKey, TValue> Create(
+            ItIndexElementFactory tIndexElementFactory,
+            IFhirDateTimeComparer FhirDateTimeComparer);
+    }
+}
