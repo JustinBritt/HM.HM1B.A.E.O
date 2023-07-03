2bf7711ad66ff2ab2cc409060c25b339cf614058
Justin Britt <britt@uwindsor.ca>
2023-07-03 02:49:12 -0400

    Added factory

---
+++ HM.HM1B.A.E.O/Factories/Contexts/SurgeonPenaltyWeightsVisitorFactory.cs
@@ -0,0 +1,47 @@
+﻿namespace HM.HM1B.A.E.O.Factories.Contexts
+{
+    using System;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using HM.HM1B.A.E.O.Interfaces.Indices;
+    using HM.HM1B.A.E.O.InterfacesFactories.Contexts;
+    using HM.HM1B.A.E.O.InterfacesFactories.ParameterElements.SurgeonPenaltyWeights;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Contexts;
+    using HM.HM1B.A.E.O.Visitors.Contexts;
+
+    internal sealed class SurgeonPenaltyWeightsVisitorFactory : ISurgeonPenaltyWeightsVisitorFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public SurgeonPenaltyWeightsVisitorFactory()
+        {
+        }
+
+        public ISurgeonPenaltyWeightsVisitor<TKey, TValue> Create<TKey, TValue>(
+            IωParameterElementFactory ωParameterElementFactory,
+            Is s)
+            where TKey : Organization
+            where TValue : INullableValue<decimal>
+        {
+            ISurgeonPenaltyWeightsVisitor<TKey, TValue> instance = null;
+
+            try
+            {
+                instance = new SurgeonPenaltyWeightsVisitor<TKey, TValue>(
+                    ωParameterElementFactory,
+                    s);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ HM.HM1B.A.E.O/InterfacesFactories/Contexts/ISurgeonPenaltyWeightsVisitorFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace HM.HM1B.A.E.O.InterfacesFactories.Contexts
+{
+    using Hl7.Fhir.Model;
+
+    using HM.HM1B.A.E.O.Interfaces.Indices;
+    using HM.HM1B.A.E.O.InterfacesFactories.ParameterElements.SurgeonPenaltyWeights;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Contexts;
+
+    public interface ISurgeonPenaltyWeightsVisitorFactory
+    {
+        ISurgeonPenaltyWeightsVisitor<TKey, TValue> Create<TKey, TValue>(
+            IωParameterElementFactory ωParameterElementFactory,
+            Is s)
+            where TKey : Organization
+            where TValue : INullableValue<decimal>;
+    }
+}
