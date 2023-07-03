9ebe2672d6e1fc10a11fabc70f42e38cea2ea831
Justin Britt <britt@uwindsor.ca>
2023-07-03 07:19:51 -0400

    Added factory

---
+++ HM.HM1B.A.E.O/Factories/Contexts/SurgeonScenarioMaximumNumberPatientsInnerVisitorFactory.cs
@@ -0,0 +1,50 @@
+﻿namespace HM.HM1B.A.E.O.Factories.Contexts
+{
+    using System;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using HM.HM1B.A.E.O.Interfaces.IndexElements;
+    using HM.HM1B.A.E.O.Interfaces.Indices;
+    using HM.HM1B.A.E.O.InterfacesFactories.Contexts;
+    using HM.HM1B.A.E.O.InterfacesFactories.ParameterElements.Surgeries;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Contexts;
+    using HM.HM1B.A.E.O.Visitors.Contexts;
+
+    internal sealed class SurgeonScenarioMaximumNumberPatientsInnerVisitorFactory : ISurgeonScenarioMaximumNumberPatientsInnerVisitorFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public SurgeonScenarioMaximumNumberPatientsInnerVisitorFactory()
+        {
+        }
+
+        public ISurgeonScenarioMaximumNumberPatientsInnerVisitor<TKey, TValue> Create<TKey, TValue>(
+            InParameterElementFactory nParameterElementFactory,
+            IsIndexElement sIndexElement,
+            IΛ Λ)
+            where TKey : INullableValue<int>
+            where TValue : INullableValue<int>
+        {
+            ISurgeonScenarioMaximumNumberPatientsInnerVisitor<TKey, TValue> instance = null;
+
+            try
+            {
+                instance = new SurgeonScenarioMaximumNumberPatientsInnerVisitor<TKey, TValue>(
+                    nParameterElementFactory,
+                    sIndexElement,
+                    Λ);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ HM.HM1B.A.E.O/InterfacesFactories/Contexts/ISurgeonScenarioMaximumNumberPatientsInnerVisitorFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace HM.HM1B.A.E.O.InterfacesFactories.Contexts
+{
+    using Hl7.Fhir.Model;
+
+    using HM.HM1B.A.E.O.Interfaces.IndexElements;
+    using HM.HM1B.A.E.O.Interfaces.Indices;
+    using HM.HM1B.A.E.O.InterfacesFactories.ParameterElements.Surgeries;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Contexts;
+
+    public interface ISurgeonScenarioMaximumNumberPatientsInnerVisitorFactory
+    {
+        ISurgeonScenarioMaximumNumberPatientsInnerVisitor<TKey, TValue> Create<TKey, TValue>(
+            InParameterElementFactory nParameterElementFactory,
+            IsIndexElement sIndexElement,
+            IΛ Λ)
+            where TKey : INullableValue<int>
+            where TValue : INullableValue<int>;
+    }
+}
