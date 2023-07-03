d9e67fcf89e091018fd6ae6677a0f87505274d0e
Justin Britt <britt@uwindsor.ca>
2023-07-03 07:31:27 -0400

    Added factory

---
+++ HM.HM1B.A.E.O/Factories/Contexts/SurgeonServiceLevelNumberTimeBlocksInnerVisitorFactory.cs
@@ -0,0 +1,50 @@
+﻿namespace HM.HM1B.A.E.O.Factories.Contexts
+{
+    using System;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using HM.HM1B.A.E.O.Interfaces.IndexElements;
+    using HM.HM1B.A.E.O.Interfaces.Indices;
+    using HM.HM1B.A.E.O.InterfacesFactories.Contexts;
+    using HM.HM1B.A.E.O.InterfacesFactories.ParameterElements.SurgeonServiceLevelTimeBlockRequirements;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Contexts;
+    using HM.HM1B.A.E.O.Visitors.Contexts;
+
+    internal sealed class SurgeonServiceLevelNumberTimeBlocksInnerVisitorFactory : ISurgeonServiceLevelNumberTimeBlocksInnerVisitorFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public SurgeonServiceLevelNumberTimeBlocksInnerVisitorFactory()
+        {
+        }
+
+        public ISurgeonServiceLevelNumberTimeBlocksInnerVisitor<TKey, TValue> Create<TKey, TValue>(
+            IAParameterElementFactory AParameterElementFactory,
+            IsIndexElement sIndexElement,
+            Iυ1 υ1)
+            where TKey : INullableValue<int>
+            where TValue : INullableValue<int>
+        {
+            ISurgeonServiceLevelNumberTimeBlocksInnerVisitor<TKey, TValue> instance = null;
+
+            try
+            {
+                instance = new SurgeonServiceLevelNumberTimeBlocksInnerVisitor<TKey, TValue>(
+                    AParameterElementFactory,
+                    sIndexElement,
+                    υ1);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ HM.HM1B.A.E.O/InterfacesFactories/Contexts/ISurgeonServiceLevelNumberTimeBlocksInnerVisitorFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace HM.HM1B.A.E.O.InterfacesFactories.Contexts
+{
+    using Hl7.Fhir.Model;
+
+    using HM.HM1B.A.E.O.Interfaces.IndexElements;
+    using HM.HM1B.A.E.O.Interfaces.Indices;
+    using HM.HM1B.A.E.O.InterfacesFactories.ParameterElements.SurgeonServiceLevelTimeBlockRequirements;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Contexts;
+
+    public interface ISurgeonServiceLevelNumberTimeBlocksInnerVisitorFactory
+    {
+        ISurgeonServiceLevelNumberTimeBlocksInnerVisitor<TKey, TValue> Create<TKey, TValue>(
+            IAParameterElementFactory AParameterElementFactory,
+            IsIndexElement sIndexElement,
+            Iυ1 υ1)
+            where TKey : INullableValue<int>
+            where TValue : INullableValue<int>;
+    }
+}
