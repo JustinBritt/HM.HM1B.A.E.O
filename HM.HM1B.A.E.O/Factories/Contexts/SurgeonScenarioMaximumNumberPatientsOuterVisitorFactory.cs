4ef497062fe7946691def7fff0415ee4562f7639
Justin Britt <britt@uwindsor.ca>
2023-07-03 04:48:44 -0400

    Added factory

---
+++ HM.HM1B.A.E.O/Factories/Contexts/SurgeonScenarioMaximumNumberPatientsOuterVisitorFactory.cs
@@ -0,0 +1,51 @@
+﻿namespace HM.HM1B.A.E.O.Factories.Contexts
+{
+    using System;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using HM.HM1B.A.E.O.Interfaces.Indices;
+    using HM.HM1B.A.E.O.InterfacesFactories.Contexts;
+    using HM.HM1B.A.E.O.InterfacesFactories.ParameterElements.Surgeries;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Contexts;
+    using HM.HM1B.A.E.O.Visitors.Contexts;
+   
+    internal sealed class SurgeonScenarioMaximumNumberPatientsOuterVisitorFactory : ISurgeonScenarioMaximumNumberPatientsOuterVisitorFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public SurgeonScenarioMaximumNumberPatientsOuterVisitorFactory()
+        {
+        }
+
+        public ISurgeonScenarioMaximumNumberPatientsOuterVisitor<TKey, TValue> Create<TKey, TValue>(
+            InParameterElementFactory nParameterElementFactory,
+            Is s,
+            IΛ Λ)
+            where TKey : Organization
+            where TValue : RedBlackTree<INullableValue<int>, INullableValue<int>>
+        {
+            ISurgeonScenarioMaximumNumberPatientsOuterVisitor<TKey, TValue> instance = null;
+
+            try
+            {
+                instance = new SurgeonScenarioMaximumNumberPatientsOuterVisitor<TKey, TValue>(
+                    nParameterElementFactory,
+                    s,
+                    Λ);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ HM.HM1B.A.E.O/InterfacesFactories/Contexts/ISurgeonScenarioMaximumNumberPatientsOuterVisitorFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace HM.HM1B.A.E.O.InterfacesFactories.Contexts
+{
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using HM.HM1B.A.E.O.Interfaces.Indices;
+    using HM.HM1B.A.E.O.InterfacesFactories.ParameterElements.Surgeries;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Contexts;
+
+    public interface ISurgeonScenarioMaximumNumberPatientsOuterVisitorFactory
+    {
+        ISurgeonScenarioMaximumNumberPatientsOuterVisitor<TKey, TValue> Create<TKey, TValue>(
+            InParameterElementFactory nParameterElementFactory,
+            Is s,
+            IΛ Λ)
+            where TKey : Organization
+            where TValue : RedBlackTree<INullableValue<int>, INullableValue<int>>;
+    }
+}
