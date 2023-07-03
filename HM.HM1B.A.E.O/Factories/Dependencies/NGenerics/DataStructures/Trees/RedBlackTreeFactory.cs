66e06c0b2f9958df69c85ed1d36ad1a064917071
Justin Britt <britt@uwindsor.ca>
2023-07-03 08:34:43 -0400

    Added factory

---
+++ HM.HM1B.A.E.O/Factories/Dependencies/NGenerics/DataStructures/Trees/RedBlackTreeFactory.cs
@@ -0,0 +1,37 @@
+﻿namespace HM.HM1B.A.E.O.Factories.Dependencies.NGenerics.DataStructures.Trees
+{
+    using System;
+
+    using log4net;
+
+    using global::NGenerics.DataStructures.Trees;
+
+    using HM.HM1B.A.E.O.InterfacesFactories.Dependencies.NGenerics.DataStructures.Trees;
+
+    internal sealed class RedBlackTreeFactory : IRedBlackTreeFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public RedBlackTreeFactory()
+        {
+        }
+
+        public RedBlackTree<TKey, TValue> Create<TKey, TValue>()
+        {
+            RedBlackTree<TKey, TValue> model = null;
+
+            try
+            {
+                model = new RedBlackTree<TKey, TValue>();
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return model;
+        }
+    }
+}
---
+++ HM.HM1B.A.E.O/InterfacesFactories/Dependencies/NGenerics/DataStructures/Trees/IRedBlackTreeFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace HM.HM1B.A.E.O.InterfacesFactories.Dependencies.NGenerics.DataStructures.Trees
+{
+    using global::NGenerics.DataStructures.Trees;
+
+    public interface IRedBlackTreeFactory
+    {
+        RedBlackTree<TKey, TValue> Create<TKey, TValue>();
+    }
+}
