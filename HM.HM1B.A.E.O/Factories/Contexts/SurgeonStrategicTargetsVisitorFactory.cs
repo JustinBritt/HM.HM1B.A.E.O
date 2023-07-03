2da4cc2c9070586f7e70f1bdc754e60bddf945b4
Justin Britt <britt@uwindsor.ca>
2023-07-03 03:02:29 -0400

    Added factory

---
+++ HM.HM1B.A.E.O/Factories/Contexts/SurgeonStrategicTargetsVisitorFactory.cs
@@ -0,0 +1,47 @@
+﻿namespace HM.HM1B.A.E.O.Factories.Contexts
+{
+    using System;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using HM.HM1B.A.E.O.Interfaces.Indices;
+    using HM.HM1B.A.E.O.InterfacesFactories.Contexts;
+    using HM.HM1B.A.E.O.InterfacesFactories.ParameterElements.SurgeonStrategicTargets;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Contexts;
+    using HM.HM1B.A.E.O.Visitors.Contexts;
+
+    internal sealed class SurgeonStrategicTargetsVisitorFactory : ISurgeonStrategicTargetsVisitorFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public SurgeonStrategicTargetsVisitorFactory()
+        {
+        }
+
+        public ISurgeonStrategicTargetsVisitor<TKey, TValue> Create<TKey, TValue>(
+            INParameterElementFactory NParameterElementFactory,
+            Is s)
+            where TKey : Organization
+            where TValue : INullableValue<int>
+        {
+            ISurgeonStrategicTargetsVisitor<TKey, TValue> instance = null;
+
+            try
+            {
+                instance = new SurgeonStrategicTargetsVisitor<TKey, TValue>(
+                    NParameterElementFactory,
+                    s);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ HM.HM1B.A.E.O/InterfacesFactories/Contexts/ISurgeonStrategicTargetsVisitorFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace HM.HM1B.A.E.O.InterfacesFactories.Contexts
+{
+    using Hl7.Fhir.Model;
+
+    using HM.HM1B.A.E.O.Interfaces.Indices;
+    using HM.HM1B.A.E.O.InterfacesFactories.ParameterElements.SurgeonStrategicTargets;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Contexts;
+
+    public interface ISurgeonStrategicTargetsVisitorFactory
+    {
+        ISurgeonStrategicTargetsVisitor<TKey, TValue> Create<TKey, TValue>(
+            INParameterElementFactory NParameterElementFactory,
+            Is s)
+            where TKey : Organization
+            where TValue : INullableValue<int>;
+    }
+}
