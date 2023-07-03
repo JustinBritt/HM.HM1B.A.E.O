f920263747c490e42d87252b004489fbf8eee989
Justin Britt <britt@uwindsor.ca>
2023-07-03 05:13:08 -0400

    Added factory

---
+++ HM.HM1B.A.E.O/Factories/Contexts/SurgeonServiceLevelNumberTimeBlocksOuterVisitorFactory.cs
@@ -0,0 +1,51 @@
+﻿namespace HM.HM1B.A.E.O.Factories.Contexts
+{
+    using System;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using HM.HM1B.A.E.O.Interfaces.Indices;
+    using HM.HM1B.A.E.O.InterfacesFactories.Contexts;
+    using HM.HM1B.A.E.O.InterfacesFactories.ParameterElements.SurgeonServiceLevelTimeBlockRequirements;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Contexts;
+    using HM.HM1B.A.E.O.Visitors.Contexts;
+
+    internal sealed class SurgeonServiceLevelNumberTimeBlocksOuterVisitorFactory : ISurgeonServiceLevelNumberTimeBlocksOuterVisitorFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public SurgeonServiceLevelNumberTimeBlocksOuterVisitorFactory()
+        {
+        }
+
+        public ISurgeonServiceLevelNumberTimeBlocksOuterVisitor<TKey, TValue> Create<TKey, TValue>(
+            IAParameterElementFactory AParameterElementFactory,
+            Is s,
+            Iυ1 υ1)
+            where TKey : Organization
+            where TValue : RedBlackTree<INullableValue<int>, INullableValue<int>>
+        {
+            ISurgeonServiceLevelNumberTimeBlocksOuterVisitor<TKey, TValue> instance = null;
+
+            try
+            {
+                instance = new SurgeonServiceLevelNumberTimeBlocksOuterVisitor<TKey, TValue>(
+                    AParameterElementFactory,
+                    s,
+                    υ1);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ HM.HM1B.A.E.O/InterfacesFactories/Contexts/ISurgeonServiceLevelNumberTimeBlocksOuterVisitorFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace HM.HM1B.A.E.O.InterfacesFactories.Contexts
+{
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using HM.HM1B.A.E.O.Interfaces.Indices;
+    using HM.HM1B.A.E.O.InterfacesFactories.ParameterElements.SurgeonServiceLevelTimeBlockRequirements;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Contexts;
+
+    public interface ISurgeonServiceLevelNumberTimeBlocksOuterVisitorFactory
+    {
+        ISurgeonServiceLevelNumberTimeBlocksOuterVisitor<TKey, TValue> Create<TKey, TValue>(
+            IAParameterElementFactory AParameterElementFactory,
+            Is s,
+            Iυ1 υ1)
+            where TKey : Organization
+            where TValue : RedBlackTree<INullableValue<int>, INullableValue<int>>;
+    }
+}
