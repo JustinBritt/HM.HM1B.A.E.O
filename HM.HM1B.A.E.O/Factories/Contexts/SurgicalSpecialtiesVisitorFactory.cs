f0cb905e0b0107458dd1a200a7752c0e949b6534
Justin Britt <britt@uwindsor.ca>
2023-07-03 03:12:22 -0400

    Added factory

---
+++ HM.HM1B.A.E.O/Factories/Contexts/SurgicalSpecialtiesVisitorFactory.cs
@@ -0,0 +1,50 @@
+﻿namespace HM.HM1B.A.E.O.Factories.Contexts
+{
+    using System;
+    using System.Collections.Immutable;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using HM.HM1B.A.E.O.Interfaces.Indices;
+    using HM.HM1B.A.E.O.InterfacesFactories.Contexts;
+    using HM.HM1B.A.E.O.InterfacesFactories.ParameterElements.SurgicalSpecialties;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Contexts;
+    using HM.HM1B.A.E.O.Visitors.Contexts;
+    
+    internal sealed class SurgicalSpecialtiesVisitorFactory : ISurgicalSpecialtiesVisitorFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public SurgicalSpecialtiesVisitorFactory()
+        {
+        }
+
+        public ISurgicalSpecialtiesVisitor<TKey, TValue> Create<TKey, TValue>(
+            IΔParameterElementFactory ΔParameterElementFactory,
+            Ij j,
+            Is s)
+            where TKey : Organization
+            where TValue : IImmutableSet<Organization>
+        {
+            ISurgicalSpecialtiesVisitor<TKey, TValue> instance = null;
+
+            try
+            {
+                instance = new SurgicalSpecialtiesVisitor<TKey, TValue>(
+                    ΔParameterElementFactory,
+                    j,
+                    s);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ HM.HM1B.A.E.O/InterfacesFactories/Contexts/ISurgicalSpecialtiesVisitorFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace HM.HM1B.A.E.O.InterfacesFactories.Contexts
+{
+    using System.Collections.Immutable;
+
+    using Hl7.Fhir.Model;
+
+    using HM.HM1B.A.E.O.Interfaces.Indices;
+    using HM.HM1B.A.E.O.InterfacesFactories.ParameterElements.SurgicalSpecialties;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Contexts;
+
+    public interface ISurgicalSpecialtiesVisitorFactory
+    {
+        ISurgicalSpecialtiesVisitor<TKey, TValue> Create<TKey, TValue>(
+            IΔParameterElementFactory ΔParameterElementFactory,
+            Ij j,
+            Is s)
+            where TKey : Organization
+            where TValue : IImmutableSet<Organization>;
+    }
+}
