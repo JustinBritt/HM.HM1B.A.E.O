2edf38460cf24f8662dfa340c615ac0f55ec7456
Justin Britt <britt@uwindsor.ca>
2023-07-03 06:03:50 -0400

    Added factory

---
+++ HM.HM1B.A.E.O/Factories/Results/SurgeonNumberAssignedTimeBlocks/BVisitorFactory.cs
@@ -0,0 +1,47 @@
+﻿namespace HM.HM1B.A.E.O.Factories.Results.SurgeonNumberAssignedTimeBlocks
+{
+    using System;
+
+    using log4net;
+
+    using HM.HM1B.A.E.O.Interfaces.Comparers;
+    using HM.HM1B.A.E.O.Interfaces.IndexElements;
+    using HM.HM1B.A.E.O.Interfaces.ResultElements.SurgeonNumberAssignedTimeBlocks;
+    using HM.HM1B.A.E.O.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+    using HM.HM1B.A.E.O.InterfacesFactories.Results.SurgeonNumberAssignedTimeBlocks;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Results.SurgeonNumberAssignedTimeBlocks;
+    using HM.HM1B.A.E.O.Visitors.Results.SurgeonNumberAssignedTimeBlocks;
+
+    internal sealed class BVisitorFactory : IBVisitorFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public BVisitorFactory()
+        {
+        }
+
+        public IBVisitor<TKey, TValue> Create<TKey, TValue>(
+            INullableValueFactory nullableValueFactory,
+            IOrganizationComparer organizationComparer)
+            where TKey : IsIndexElement
+            where TValue : IBResultElement
+        {
+            IBVisitor<TKey, TValue> instance = null;
+
+            try
+            {
+                instance = new BVisitor<TKey, TValue>(
+                    nullableValueFactory,
+                    organizationComparer);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ HM.HM1B.A.E.O/InterfacesFactories/Results/SurgeonNumberAssignedTimeBlocks/IBVisitorFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace HM.HM1B.A.E.O.InterfacesFactories.Results.SurgeonNumberAssignedTimeBlocks
+{
+    using HM.HM1B.A.E.O.Interfaces.Comparers;
+    using HM.HM1B.A.E.O.Interfaces.IndexElements;
+    using HM.HM1B.A.E.O.Interfaces.ResultElements.SurgeonNumberAssignedTimeBlocks;
+    using HM.HM1B.A.E.O.InterfacesFactories.Dependencies.Hl7.Fhir.R4.Model;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Results.SurgeonNumberAssignedTimeBlocks;
+
+    public interface IBVisitorFactory
+    {
+        IBVisitor<TKey, TValue> Create<TKey, TValue>(
+            INullableValueFactory nullableValueFactory,
+            IOrganizationComparer organizationComparer)
+            where TKey : IsIndexElement
+            where TValue : IBResultElement;
+    }
+}
