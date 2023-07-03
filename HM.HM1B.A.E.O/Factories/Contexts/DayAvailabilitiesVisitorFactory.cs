086e014dcaaf19fa744d69378cf330ea1f80e63a
Justin Britt <britt@uwindsor.ca>
2023-07-03 01:55:30 -0400

    Added factory

---
+++ HM.HM1B.A.E.O/Factories/Contexts/DayAvailabilitiesVisitorFactory.cs
@@ -0,0 +1,47 @@
+﻿namespace HM.HM1B.A.E.O.Factories.Contexts
+{
+    using System;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using HM.HM1B.A.E.O.Interfaces.Indices;
+    using HM.HM1B.A.E.O.InterfacesFactories.Contexts;
+    using HM.HM1B.A.E.O.InterfacesFactories.ParameterElements.DayAvailabilities;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Contexts;
+    using HM.HM1B.A.E.O.Visitors.Contexts;
+
+    internal sealed class DayAvailabilitiesVisitorFactory : IDayAvailabilitiesVisitorFactory
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public DayAvailabilitiesVisitorFactory()
+        {
+        }
+
+        public IDayAvailabilitiesVisitor<TKey, TValue> Create<TKey, TValue>(
+            IψParameterElementFactory ψParameterElementFactory,
+            It t)
+            where TKey : FhirDateTime
+            where TValue : INullableValue<bool>
+        {
+            IDayAvailabilitiesVisitor<TKey, TValue> instance = null;
+
+            try
+            {
+                instance = new DayAvailabilitiesVisitor<TKey, TValue>(
+                    ψParameterElementFactory,
+                    t);
+            }
+            catch (Exception exception)
+            {
+                this.Log.Error(
+                    exception.Message,
+                    exception);
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ HM.HM1B.A.E.O/InterfacesFactories/Contexts/IDayAvailabilitiesVisitorFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace HM.HM1B.A.E.O.InterfacesFactories.Contexts
+{
+    using Hl7.Fhir.Model;
+
+    using HM.HM1B.A.E.O.Interfaces.Indices;
+    using HM.HM1B.A.E.O.InterfacesFactories.ParameterElements.DayAvailabilities;
+    using HM.HM1B.A.E.O.InterfacesVisitors.Contexts;
+
+    public interface IDayAvailabilitiesVisitorFactory
+    {
+        IDayAvailabilitiesVisitor<TKey, TValue> Create<TKey, TValue>(
+            IψParameterElementFactory ψParameterElementFactory,
+            It t)
+            where TKey : FhirDateTime
+            where TValue : INullableValue<bool>;
+    }
+}
